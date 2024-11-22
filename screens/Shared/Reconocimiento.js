d6d18e12007fa1489bcaa8c279a50f21f535dcb6
corteantonio <antonio.cortes.sotelo@gmail.com>
2024-11-21 22:12:52 -0300

    se carga reconocimiento de objetos por iframe

--- screens/Shared/Reconocimiento.js
+++ screens/Shared/Reconocimiento.js
@@ -1,31 +1,58 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View,TouchableOpacity, Text, } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-const Reconocimiento = () => {
-  return (
-    <View style={styles.container}>
-      {/* WebView cargando una URL */}
-      <WebView 
-        source={{ uri: 'https://reconqs.000.pe/' }} 
-        style={styles.webview}
-        cacheEnabled={false}
+const Reconocimiento = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            {/* WebView cargando una URL */}
+            <WebView
+                source={{ uri: 'https://reconqs.000.pe' }}
+                style={styles.webview}
 
-      />
-    </View>
-  );
+            />
+
+
+            {/* Botón para retroceder */}
+            <TouchableOpacity style={styles.ButtonCirculoAtras} onPress={() => navigation.goBack()}>
+                <View style={styles.CirculoAtras}>
+                    <View style={{ flexDirection: 'row', marginTop: 15 }}>
+                        <Text style={{ color: 'white', fontSize: 28 }}>Atrás</Text>
+                    </View>
+                    <Text style={{ color: 'white', fontSize: 18 }}>Ver Producto</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor:'#260202'
-  },
-  webview: {
-    flex: 1,
-    marginBottom:30,
-    marginTop:30
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#260202'
+    },
+    webview: {
+        flex: 1,
+        marginBottom: 30,
+        marginTop: 30
+    }, ButtonCirculoAtras: {
+        position: 'absolute',
+        top: -15,
+        left: -15,
+        zIndex: 5
+    },
+
+    CirculoAtras: {
+        width: 150,
+        height: 150,
+        borderRadius: 125,
+        backgroundColor: '#270403',
+        display: 'flex',
+
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
 });
 
 export default Reconocimiento;
